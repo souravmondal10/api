845458acb7a5a92d49c27b542bf2cf9406b27ef7
sourav mondal <souravmondal10@gmail.com>
2021-06-14 14:40:56 +0000

    nginx config change

--- showUsers.php
+++ showUsers.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . '/config.php';
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Content-type: application/json');
 $source = 'database';
 if (isset($_GET['source'])) {
