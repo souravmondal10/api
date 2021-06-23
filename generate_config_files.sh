f60150e167c3ff2688de5e1abbfa56b1fe723cd2
sourav mondal <souravmondal10@gmail.com>
2021-06-23 10:09:08 +0000

    added the tervis CI config file

--- generate_config_files.sh
+++ generate_config_files.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-
 cp config-sample.php config.php
-
 sed -i "s/redis_host_xxx/$REDIS_HOST/g" config.php
 sed -i "s/redis_port_xxx/$REDIS_PORT/g" config.php
 sed -i "s/mysql_host_xxx/$MYSQL_HOST/g" config.php
