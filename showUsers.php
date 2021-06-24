3c1cd4c16a0e18c0ed17dd5cc92f5beedbbf43cb
sourav mondal <souravmondal10@gmail.com>
2021-06-24 08:25:27 +0000

    latest changes

--- showUsers.php
+++ showUsers.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once __DIR__ . '/config.php';
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
